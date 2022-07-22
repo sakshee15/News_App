d26bcaf8f8a0c6e3d2b529025a9f2a02da5b673d
sakshee15 <sakshee.agrawal21@vit.edu>
2022-07-22 12:36:32 +0530

    adding card component

---
+++ news_application/src/components/card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import image from './newsimage.jpg'
+import './card.css';
+
+function Card(){
+    return (
+        
+        <div className='card'>
+          <img className='card-img-top' src={image}/>
+          <div >
+            <h4 className='card-title'><b>Card title</b></h4>
+            <p className='card-text' >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className='card-date'>Date and Time</p>
+            <button className='button'>Read more</button>
+          </div>
+        </div> 
+         );
+    
+}
+export default Card;
