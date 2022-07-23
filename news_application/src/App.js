ffda35911849b777de5cc0b695c4d59ae10a5c3d
sakshee15 <sakshee.agrawal21@vit.edu>
2022-07-23 14:09:24 +0530

    adding card(component)

--- news_application/src/App.js
+++ news_application/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/card'
+
+
 function App() {
   return (
     <div className="App">
